8a6f7eedb86c81ef073cb0a74c526b14f63e0a5f
bzzlghtyr <buzzletiyear@gmail.com>
2025-02-16 14:28:58 -0500

    fixed image regeneration

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// -------------------------
+// Shuffle Array
+// -------------------------
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -6,6 +9,9 @@ function shuffleArray(array) {
   return array;
 }
 
+// -------------------------
+// Global Variables
+// -------------------------
 var lstMember = [];
 var parent = [];
 var equal = [];
@@ -17,11 +23,13 @@ var numQuestion;
 var totalSize;
 var finishSize;
 var finishKit;
+var cachedImageUrl = null;  // For caching the generated image
 
+// -------------------------
+// Initialize Ranking List
+// -------------------------
 function initList() {
-  var n = 0;
-  var mid;
-  var i;
+  var n = 0, mid, i;
   shuffleArray(namMember);
   lstMember[n] = [];
   for (i = 0; i < namMember.length; i++) {
@@ -43,12 +51,12 @@ function initList() {
       n++;
     }
   }
-  for (i = 0; i < namMember.length; i++) {
-    rec[i] = 0;
+  for (i = 0; i < namMember.length; i++) { 
+    rec[i] = 0; 
   }
   nrec = 0;
-  for (i = 0; i <= namMember.length; i++) {
-    equal[i] = -1;
+  for (i = 0; i <= namMember.length; i++) { 
+    equal[i] = -1; 
   }
   cmp1 = lstMember.length - 2;
   cmp2 = lstMember.length - 1;
@@ -59,9 +67,11 @@ function initList() {
   finishKit = 0;
 }
 
+// -------------------------
+// Sorting Logic
+// -------------------------
 function sortList(kit) {
   var i;
-  var str;
   if (kit < 0) {
     rec[nrec] = lstMember[cmp1][head1];
     head1++;
@@ -140,9 +150,14 @@ function sortList(kit) {
     }
   }
   if (cmp1 < 0) {
-    str = "Matchup #" + (numQuestion - 1) + "<br>" + Math.floor(finishSize * 100 / totalSize) + "% sorted.";
+    var str = "Matchup #" + (numQuestion - 1) + "<br>" + Math.floor(finishSize * 100 / totalSize) + "% sorted.";
     document.getElementById("matchupNumber").innerHTML = str;
     document.getElementById("progressBar").style.display = "none";
+    
+    // Log final ranking immediately when sorting is complete
+    var finalRanking = JSON.stringify(lstMember[0]);
+    logResult(finalRanking);
+    
     showResult();
     finishKit = 1;
   } else {
@@ -150,6 +165,9 @@ function sortList(kit) {
   }
 }
 
+// -------------------------
+// Display Standard Results
+// -------------------------
 function showResult() {
   var processedNamMember = namMember.map(item =>
     item.replace(/\/png\//g, "/png/150x/").replace("2025 ", "")
@@ -158,8 +176,6 @@ function showResult() {
   var sameRank = 1;
   var str = "";
   var i;
-
-  // Muted HSL colors
   var colors = [
     "hsl(120,40%,80%)", "hsl(116,40%,80%)", "hsl(112,40%,80%)", "hsl(108,40%,80%)",
     "hsl(104,40%,80%)", "hsl(100,40%,80%)", "hsl(96,40%,80%)", "hsl(92,40%,80%)",
@@ -178,9 +194,7 @@ function showResult() {
   str += "<tr>";
   str += "<td colspan='8' style='text-align:right; font-size:10px; font-family:Arial; margin:0; padding:0;'>Created by: Joseph Même | @ThreeDEF</td>";
   str += "</tr>";
-  str += "<tr>";
-  str += "<td colspan='8' style='margin:0; padding:0;'><hr style='margin:0; padding:0;'></td>";
-  str += "</tr>";
+  str += "<tr><td colspan='8' style='margin:0; padding:0;'><hr style='margin:0; padding:0;'></td></tr>";
 
   for (i = 0; i < processedNamMember.length; i++) {
     if (i % 8 === 0) {
@@ -190,12 +204,10 @@ function showResult() {
     let bgColor = colors[ranking - 1];
     var teamName = memberInfo[1];
     var teamFontSize = teamName.length > 18 ? "10px" : "11px";
-
     str += "<td style='border:1px solid #00025d; margin:0; padding:2px; background-color:" + bgColor +
            "; font-size:12px; text-align:center; font-weight:bolder; font-family:Arial; white-space:nowrap;'>";
     str += "#" + ranking + ": <span style='font-size:" + teamFontSize + ";'>" + teamName + "<br>" + memberInfo[0] + "</span><br>";
     str += "<span style='font-size:8px; line-height:10px;'>" + memberInfo[2] + "</span></td>";
-
     if ((i + 1) % 8 === 0) {
       str += "</tr>";
     }
@@ -212,8 +224,6 @@ function showResult() {
     str += "</tr>";
   }
   str += "</table>";
-  
-  // Add extra space at bottom to avoid clipping
   str += "<div style='height:40px;'></div>";
 
   document.getElementById("resultsContainer").innerHTML = str;
@@ -223,35 +233,44 @@ function showResult() {
   document.getElementById("screenieButton").style.display = "block";
 }
 
-
-
-
-
+// -------------------------
+// Display Current Matchup
+// -------------------------
 function showImage() {
   var completionPercentage = Math.floor(finishSize * 100 / totalSize);
   var progressBar = '<div style="width:auto; height:30px; background-color:#f0f0f0; border:1px solid #ccc; border-radius:5px; margin:10px auto;">';
   progressBar += '<div style="width:' + completionPercentage + '%; height:100%; background-color:#4caf50; border-radius:5px;"></div>';
   progressBar += '</div>';
   var str0 = "Matchup #" + numQuestion + "<br>" + completionPercentage + "% sorted.";
-  var str1 = "<div style='width:300px; height:300px; background-color:white; margin:0 auto;'>" + namMember[lstMember[cmp1][head1]].split("|")[0] + "</div>";
-  var str2 = "<div style='width:300px; text-align:center; font-size:18px;'>" + namMember[lstMember[cmp1][head1]].split("|")[1] + "</div>";
-  var str3 = "<div style='width:300px; text-align:center; font-size:12px;'>" + namMember[lstMember[cmp1][head1]].split("|")[2] + "</div>";
-  var str4 = "<div style='width:300px; height:300px; background-color:white; margin:0 auto;'>" + namMember[lstMember[cmp2][head2]].split("|")[0] + "</div>";
-  var str5 = "<div style='width:300px; text-align:center; font-size:18px;'>" + namMember[lstMember[cmp2][head2]].split("|")[1] + "</div>";
-  var str6 = "<div style='width:300px; text-align:center; font-size:12px;'>" + namMember[lstMember[cmp2][head2]].split("|")[2] + "</div>";
   document.getElementById("matchupNumber").innerHTML = str0;
-  document.getElementById("leftField").innerHTML = str1 + str2 + str3;
-  document.getElementById("rightField").innerHTML = str4 + str5 + str6;
+  
+  var leftParts = namMember[lstMember[cmp1][head1]].split("|");
+  var rightParts = namMember[lstMember[cmp2][head2]].split("|");
+  
+  var str1 = "<div style='width:300px; height:300px; background-color:white; margin:0 auto;'>" + leftParts[0] + "</div>";
+  str1 += "<div style='width:300px; text-align:center; font-size:18px;'>" + leftParts[1] + "</div>";
+  str1 += "<div style='width:300px; text-align:center; font-size:12px;'>" + leftParts[2] + "</div>";
+
+  var str2 = "<div style='width:300px; height:300px; background-color:white; margin:0 auto;'>" + rightParts[0] + "</div>";
+  str2 += "<div style='width:300px; text-align:center; font-size:18px;'>" + rightParts[1] + "</div>";
+  str2 += "<div style='width:300px; text-align:center; font-size:12px;'>" + rightParts[2] + "</div>";
+
+  document.getElementById("leftField").innerHTML = str1;
+  document.getElementById("rightField").innerHTML = str2;
+  
   numQuestion++;
   document.getElementById("progressBar").innerHTML = progressBar;
 }
 
-function toNameFace(n) {
-  var str = namMember[n];
-  return str;
-}
-
+// -------------------------
+// Generate Shareable Image with Caching
+// -------------------------
 function generateShareableImage() {
+  if (cachedImageUrl) {
+    showModalWithImage(cachedImageUrl);
+    return;
+  }
+  
   var container = document.getElementById("resultsContainer");
   var clone = container.cloneNode(true);
   clone.style.position = 'absolute';
@@ -260,32 +279,12 @@ function generateShareableImage() {
   document.body.appendChild(clone);
   var width = clone.scrollWidth;
   var height = clone.scrollHeight;
-
-  domtoimage.toPng(clone, { width: width, height: height })
+  
+  domtoimage.toPng(clone, { width: width, height: height, scale: 2 })
     .then(function(dataUrl) {
+      cachedImageUrl = dataUrl;
       document.body.removeChild(clone);
-      var modal = document.createElement('div');
-      modal.style.position = 'fixed';
-      modal.style.zIndex = '9999';
-      modal.style.top = '0';
-      modal.style.left = '0';
-      modal.style.width = '100vw';
-      modal.style.height = '100vh';
-      modal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-      modal.style.display = 'flex';
-      modal.style.justifyContent = 'center';
-      modal.style.alignItems = 'center';
-
-      var img = new Image();
-      img.src = dataUrl;
-      img.style.maxWidth = '90%';
-      img.style.maxHeight = '90%';
-
-      modal.appendChild(img);
-      modal.addEventListener('click', function() {
-        document.body.removeChild(modal);
-      });
-      document.body.appendChild(modal);
+      showModalWithImage(dataUrl);
     })
     .catch(function(error) {
       console.error("Error generating shareable image", error);
@@ -293,112 +292,73 @@ function generateShareableImage() {
     });
 }
 
+function showModalWithImage(dataUrl) {
+  var modal = document.createElement('div');
+  modal.style.position = 'fixed';
+  modal.style.zIndex = '9999';
+  modal.style.top = '0';
+  modal.style.left = '0';
+  modal.style.width = '100vw';
+  modal.style.height = '100vh';
+  modal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  modal.style.display = 'flex';
+  modal.style.justifyContent = 'center';
+  modal.style.alignItems = 'center';
 
-/*
-// Fancy layout functions are currently commented out.
-function showFancyLayout() {
-  let processedNamMember = namMember.map(item =>
-    item.replace(/\/png\//g, "/png/150x/").replace("2025 ", "")
-  );
-  let str = `
-  <div style="font-family:Arial, sans-serif; width:1000px; margin:0 auto; text-align:center; background:#fff; border:1px solid #ccc; padding:10px;">
-    <h2 style="font-size:32px; margin-bottom:5px;">Fancy Results Layout</h2>
-    <hr style="margin:10px 0;">
-    <div style="display:flex; justify-content:space-between; margin-bottom:20px;">
-      <div style="width:48%; border:3px solid green; padding:10px;">
-        <h3 style="font-size:24px; margin:5px 0;">Best Kit</h3>`;
-  let bestIndex = lstMember[0][0];
-  let bestKit = processedNamMember[bestIndex].split("|");
-  str += `
-        <div style="font-size:14px;">
-          ${bestKit[0]}<br>
-          <b>${bestKit[1]}</b><br>
-          <i>${bestKit[2]}</i>
-        </div>
-      </div>
-      <div style="width:48%; border:3px solid red; padding:10px;">
-        <h3 style="font-size:24px; margin:5px 0;">Worst Kit</h3>`;
-  let worstIndex = lstMember[0][lstMember[0].length - 1];
-  let worstKit = processedNamMember[worstIndex].split("|");
-  str += `
-        <div style="font-size:14px;">
-          ${worstKit[0]}<br>
-          <b>${worstKit[1]}</b><br>
-          <i>${worstKit[2]}</i>
-        </div>
-      </div>
-    </div>
-    <h3 style="font-size:20px; margin-bottom:10px;">Full Ranking</h3>
-    <table style="width:100%; font-size:14px; border:0; background:#fff;" align="center">
-  `;
-  for (let i = 0; i < processedNamMember.length; i++) {
-    if (i % 4 === 0) {
-      str += "<tr>";
-    }
-    let kitInfo = processedNamMember[lstMember[0][i]].split("|");
-    str += `<td style="border:1px solid #ccc; padding:5px; text-align:center;">
-             <span style="font-size:12px; font-weight:bold;">#${i+1}</span><br>
-             ${kitInfo[0]}<br>
-             <span style="font-size:10px;">${kitInfo[1]}</span><br>
-             <span style="font-size:8px;">${kitInfo[2]}</span>
-             </td>`;
-    if ((i + 1) % 4 === 0) {
-      str += "</tr>";
-    }
-  }
-  if (processedNamMember.length % 4 !== 0) {
-    str += "</tr>";
-  }
-  str += "</table></div>";
-  document.getElementById("fancyResultsContainer").innerHTML = str;
-  document.getElementById("fancyResultsContainer").style.display = "block";
-  document.getElementById("fancyScreenieButton").innerHTML = "<br><button onclick='generateFancyShareableImage()' style='font-size:20px; padding:10px 20px; background-color:#ccc; border:none; border-radius:5px'>Show Fancy Image</button>";
-  document.getElementById("fancyScreenieButton").style.display = "block";
+  var img = new Image();
+  img.src = dataUrl;
+  img.style.maxWidth = '90%';
+  img.style.maxHeight = '90%';
+
+  modal.appendChild(img);
+  modal.addEventListener('click', function() {
+    document.body.removeChild(modal);
+  });
+  document.body.appendChild(modal);
 }
 
-function generateFancyShareableImage() {
-  let container = document.getElementById("fancyResultsContainer");
-  let width = container.scrollWidth;
-  let height = container.scrollHeight;
-  domtoimage.toPng(container, { width: width, height: height })
-    .then(function(dataUrl) {
-      let modal = document.createElement('div');
-      modal.style.position = 'fixed';
-      modal.style.zIndex = '9999';
-      modal.style.top = '0';
-      modal.style.left = '0';
-      modal.style.width = '100vw';
-      modal.style.height = '100vh';
-      modal.style.backgroundColor = 'rgba(0,0,0,0.5)';
-      modal.style.display = 'flex';
-      modal.style.justifyContent = 'center';
-      modal.style.alignItems = 'center';
-      let img = new Image();
-      img.src = dataUrl;
-      img.style.maxWidth = '90%';
-      img.style.maxHeight = '90%';
-      modal.appendChild(img);
-      modal.addEventListener('click', function() {
-        document.body.removeChild(modal);
-      });
-      document.body.appendChild(modal);
-    })
-    .catch(function(error) {
-      console.error("Error generating fancy shareable image", error);
-    });
+// -------------------------
+// Log Final Result to Google Form
+// -------------------------
+function logResult(resultData) {
+  if (localStorage.getItem("rankingLogged")) return;
+  var uniqueID = localStorage.getItem("uniqueID") || Date.now().toString();
+  localStorage.setItem("uniqueID", uniqueID);
+
+  var formData = new FormData();
+  // Replace these with your actual Google Form field IDs
+  formData.append("entry.1203843018", resultData);
+  formData.append("entry.1862921466", uniqueID);
+
+  var googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSfd63tHUzTeRVjnBeRX3z7Y87wfH38W9NzjgXmgQV8Hgv_MKg/formResponse";
+
+  fetch(googleFormURL, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData
+  }).then(function() {
+    localStorage.setItem("rankingLogged", "true");
+    console.log("Result logged successfully (no-cors).");
+  }).catch(function(error) {
+    console.error("Error logging result:", error);
+  });
 }
-*/
-// shortcut to finish ranking quickly for testing
+
+// -------------------------
+// Hidden Shortcut for Testing
+// -------------------------
 document.addEventListener("keydown", function(e) {
   if (e.ctrlKey && e.shiftKey && e.key === "F") {
-    // Only trigger if ranking is not yet finished
     if (cmp1 >= 0) {
-      finishKit = 1; // simulate finishing
-      showResult();
+      finishKit = 1;
+      sortList(-1);
       console.log("Hidden shortcut triggered: finishing ranking.");
     }
   }
 });
 
+// -------------------------
+// Initialize and Show First Matchup
+// -------------------------
 initList();
 showImage();
